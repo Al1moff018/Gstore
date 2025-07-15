e1b07e579e8162e5ab657f309e7a9c2d5518cd7c
Al1moff018 <alimovmk018@gmail.com>
2025-07-16 02:02:40 +0500

    kasdkasjdkasdj

--- Js/script.js
+++ Js/script.js
@@ -10,3 +10,12 @@
         clickable: true,
       },
     });
+    var swiper = new Swiper(".bottom-swiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
