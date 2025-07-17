106205f21d52507ae53668fe48e9101f18309322
Al1moff018 <alimovmk018@gmail.com>
2025-07-18 04:02:49 +0500

    aaaaaaaaaaaaaaa

--- Js/script.js
+++ Js/script.js
@@ -10,12 +10,25 @@
         clickable: true,
       },
     });
-    var swiper = new Swiper(".bottom-swiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
+    var swiper = new Swiper(".center-swiper", {
+      slidesPerView: 2.6,
+      spaceBetween: 16,
       freeMode: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+      },
+    });
+    var swiper = new Swiper(".bottom-swiper", {
+      slidesPerView: 2.4,
+      spaceBetween: 20,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
